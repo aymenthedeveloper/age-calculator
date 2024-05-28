2b839dafb8e51d6c33ad5fdc6b0650f639004d10
aymenthedeveloper <aymendev.contact@gmail.com>
2024-05-28 14:14:45 +0100

    updated main.js

--- main.js
+++ main.js
@@ -3,51 +3,29 @@ const inputMonth = document.getElementById('month');
 const inputYear = document.getElementById('year');
 
 const today = new Date();
-const currentDay = Number(today.getDate());
-const currentMonth = Number(today.getMonth()) + 1;
-const currentYear = Number(today.getFullYear());
-
-
-function isEmpty(element){
-  if (element.value == '') {
-    element.nextElementSibling.innerHTML = 'This field is required';
-    return true
-  }
-  return false
-}
+const [currentDay, currentMonth, currentYear] = [today.getDate(), today.getMonth() + 1, today.getFullYear()];
+const getInput = () => [inputDay.value, inputMonth.value, inputYear.value].map(Number);
 
 function checkDays() {
-  day = Number(inputDay.value)
-  month = Number(inputMonth.value)
-  year = Number(inputYear.value)
+  let [day, month, year] = getInput();
   inputDay.nextElementSibling.innerHTML = '';
-  var empty = isEmpty(inputDay)
-  if ((isNaN(day) || day <= 0 || day > 31) && empty != true) {
-    inputDay.nextElementSibling.innerHTML = 'Must be a valid day';
-  } else if ((day > currentDay && month == currentMonth && year == currentYear) && empty != true) {
-    inputDay.nextElementSibling.innerHTML = 'Must be in the past';
-  }
+  if (!inputDay.value) inputDay.nextElementSibling.innerHTML = 'This field is required';
+  else if (isNaN(day) || day <= 0 || day > 31) inputDay.nextElementSibling.innerHTML = 'Must be a valid day';
+  else if (day > currentDay && month == currentMonth && year == currentYear) inputDay.nextElementSibling.innerHTML = 'Must be in the past';
 }
 function checkMonths() {
-  month = Number(inputMonth.value)
-  year = Number(inputYear.value)
+  let [month, year] = getInput().slice(1,2);
   inputMonth.nextElementSibling.innerHTML = '';
-  var empty = isEmpty(inputMonth)
-  if ((isNaN(month) || month <= 0 || month > 12) && empty != true) {
-    inputMonth.nextElementSibling.innerHTML = 'Must be a valid month';
-  } else if ((month > currentMonth && year == currentYear) && empty != true) {
-    inputMonth.nextElementSibling.innerHTML = 'Must be in the past';
-  }
+  if (!inputMonth.value) inputMonth.nextElementSibling.innerHTML = 'This field is required';
+  else if (isNaN(month) || month <= 0 || month > 12) inputMonth.nextElementSibling.innerHTML = 'Must be a valid month';
+  else if (month > currentMonth && year == currentYear) inputMonth.nextElementSibling.innerHTML = 'Must be in the past';
 }
 function checkYears() {
-  year = Number(inputYear.value)
+  let year = getInput()[2];
   inputYear.nextElementSibling.innerHTML = '';
-  var empty = isEmpty(inputYear)
-  if ((isNaN(year) || year <= 0) && empty != true) {
-    inputYear.nextElementSibling.innerHTML = 'Must be a valid year';
-  } else if ((year > currentYear) && empty != true) {
-    inputYear.nextElementSibling.innerHTML = 'Must be in the past';
-  } 
+  if (!inputYear.value) inputYear.nextElementSibling.innerHTML = 'This field is required';
+  else if (isNaN(year) || year <= 0) inputYear.nextElementSibling.innerHTML = 'Must be a valid year';
+  else if (year > currentYear) inputYear.nextElementSibling.innerHTML = 'Must be in the past';
 }
 
 document.querySelector('button').addEventListener('click', () => {
@@ -55,10 +33,7 @@ document.querySelector('button').addEventListener('click', () => {
   checkMonths()
   checkYears()
   const errorSpan = document.querySelectorAll('.container .input-fields .input-group span');
-  errors = 3
-  for (let i = 0; i < errorSpan.length; i++) {
-    if (errorSpan[i].innerHTML == ''){errors -= 1}
-  }
+  const errors = Array(3).fill(0).reduce((count,v,i) => errorSpan[i].innerHTML !== ''? ++count: count, 0);
   if (errors == 0) {
     const birth = new Date(`${Number(inputYear.value)}-${Number(inputMonth.value)}-${Number(inputDay.value)}`)
     let diff =  Math.floor((today.getTime() - birth.getTime()) / (1000 * 3600 * 24));
@@ -66,5 +41,5 @@ document.querySelector('button').addEventListener('click', () => {
     document.getElementById('months').innerHTML = Math.floor((diff % 365.25) / 30.416);
     document.getElementById('days').innerHTML = Math.floor((diff % 365.25) % 30.416);
   }
-})
+});
 
